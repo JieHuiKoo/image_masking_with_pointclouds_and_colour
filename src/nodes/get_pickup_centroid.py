ea9c89e61fbae868e027637b8aac5cea81bb7440
JieHuiKoo <31171083+JieHuiKoo@users.noreply.github.com>
2023-04-05 14:37:48 +0800

    Added pointcloud

--- src/nodes/get_pickup_centroid.py
+++ src/nodes/get_pickup_centroid.py
@@ -6,6 +6,7 @@ import message_filters
 from std_msgs.msg import String
 from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import Bool
+from sensor_msgs.msg import PointCloud2
 
 from collections import Counter
 
@@ -18,6 +19,7 @@ class object_properties:
     location = PoseStamped()
     class_buffer = []
     buffer_len = 20
+    pointcloud = PointCloud2()
 
     def add_to_class_buffer(self, class_name):
         if len(self.class_buffer) >= self.buffer_len:
@@ -40,16 +42,20 @@ def convert_string_to_rosMsg(python_string):
     ros_msg.data = python_string
     return ros_msg
 
-def record_object_params(object_class_msg, object_poseStamped_msg, object_is_bb_intersecting_msg):
+def record_object_params(object_class_msg, object_poseStamped_msg, object_is_bb_intersecting_msg, object_pointcloud_msg):
     if object_is_bb_intersecting_msg:
         observed_object.add_to_class_buffer(object_class_msg)
         observed_object.location = object_poseStamped_msg
+        observed_object.pointcloud = object_pointcloud_msg
 
 def handle_get_pickup_location(req):
     rospy.loginfo("[GetPickupCentroid]: Request received")
     rospy.loginfo("[GetPickupCentroid]: Object Class: %s", observed_object.get_object_class())
 
-    return (GetPickupCentroidResponse(object_centroid=observed_object.location, object_class=convert_string_to_rosMsg(observed_object.get_object_class())))
+    return (GetPickupCentroidResponse(\
+        object_centroid=observed_object.location, \
+        object_class=convert_string_to_rosMsg(observed_object.get_object_class()), \
+        object_pointcloud=observed_object.pointcloud))
 
 def start_node():
     rospy.init_node('get_pickup_centroid')
@@ -58,10 +64,11 @@ def start_node():
     object_class = message_filters.Subscriber("armCamera/nearestObject_Class", String)
     object_poseStamped = message_filters.Subscriber("armCamera/nearestCloudCluster_Centroid", PoseStamped)
     object_is_bb_intersecting = message_filters.Subscriber("armCamera/nearestObject_Detected", Bool)
+    object_pointcloud = message_filters.Subscriber("armCamera/nearestCloudCluster", PointCloud2)
 
     # Subscribe to the 3 topics
     # Slop refers to delay we can wait for message in seconds
-    ts = message_filters.ApproximateTimeSynchronizer([object_class, object_poseStamped, object_is_bb_intersecting], queue_size=1, slop=1000000000000000000, allow_headerless=True)
+    ts = message_filters.ApproximateTimeSynchronizer([object_class, object_poseStamped, object_is_bb_intersecting, object_pointcloud], queue_size=1, slop=1000000000000000000, allow_headerless=True)
     ts.registerCallback(record_object_params)
     
     s = rospy.Service('GetPickupCentroid', GetPickupCentroid, handle_get_pickup_location)    
