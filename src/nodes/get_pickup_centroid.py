e5a3df41985b4bf050432a82b78673214ada9e12
JieHuiKoo <31171083+JieHuiKoo@users.noreply.github.com>
2023-05-06 08:09:24 +0800

    updated node

--- src/nodes/get_pickup_centroid.py
+++ src/nodes/get_pickup_centroid.py
@@ -20,6 +20,8 @@ class object_properties:
     class_buffer = []
     buffer_len = 20
     pointcloud = PointCloud2()
+    timestamp = rospy.Time()
+    valid = False
 
     def add_to_class_buffer(self, class_name):
         if len(self.class_buffer) >= self.buffer_len:
@@ -43,10 +45,25 @@ def convert_string_to_rosMsg(python_string):
     return ros_msg
 
 def record_object_params(object_class_msg, object_poseStamped_msg, object_is_bb_intersecting_msg, object_pointcloud_msg):
+    
     if object_is_bb_intersecting_msg:
-        observed_object.add_to_class_buffer(object_class_msg)
-        observed_object.location = object_poseStamped_msg
-        observed_object.pointcloud = object_pointcloud_msg
+        if (object_pointcloud_msg.height * object_pointcloud_msg.width) > 0:
+            observed_object.add_to_class_buffer(object_class_msg)
+            observed_object.class_name = observed_object.get_object_class()
+            observed_object.location = object_poseStamped_msg
+            observed_object.pointcloud = object_pointcloud_msg
+            observed_object.timestamp = object_pointcloud_msg.header.stamp
+    
+            if observed_object.class_name != "Not_Ready_Yet":
+                print("[GetPickupCentroid]: Object Valid")
+                observed_object.valid = True
+                return
+    
+    print("[GetPickupCentroid]: Object Invalid")
+    observed_object.valid = True
+    
+def is_within_reasonable_delay(observed_object):
+    return ((rospy.Time.now() - observed_object.timestamp).to_sec() < 1000)
 
 def handle_get_pickup_location(req):
     rospy.loginfo("[GetPickupCentroid]: Request received")
@@ -55,11 +72,12 @@ def handle_get_pickup_location(req):
     return (GetPickupCentroidResponse(\
         object_centroid=observed_object.location, \
         object_class=convert_string_to_rosMsg(observed_object.get_object_class()), \
-        object_pointcloud=observed_object.pointcloud))
+        object_pointcloud=observed_object.pointcloud, \
+        object_valid=observed_object.valid))
 
 def start_node():
     rospy.init_node('get_pickup_centroid')
-    rospy.loginfo('{GetPickupCentroid]: Service started')
+    rospy.loginfo('[GetPickupCentroid]: Service started')
     
     object_class = message_filters.Subscriber("armCamera/nearestObject_Class", String)
     object_poseStamped = message_filters.Subscriber("armCamera/nearestCloudCluster_Centroid", PoseStamped)
@@ -68,7 +86,7 @@ def start_node():
 
     # Subscribe to the 3 topics
     # Slop refers to delay we can wait for message in seconds
-    ts = message_filters.ApproximateTimeSynchronizer([object_class, object_poseStamped, object_is_bb_intersecting, object_pointcloud], queue_size=1, slop=1000000000000000000, allow_headerless=True)
+    ts = message_filters.ApproximateTimeSynchronizer([object_class, object_poseStamped, object_is_bb_intersecting, object_pointcloud], queue_size=1, slop=1000000000000, allow_headerless=True)
     ts.registerCallback(record_object_params)
     
     s = rospy.Service('GetPickupCentroid', GetPickupCentroid, handle_get_pickup_location)    
